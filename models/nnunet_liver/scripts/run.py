7b5bcb5c53c572c722a4a854213cfdd2fcf71a32
LennyN95 <lenny@moontec.de>
2023-05-22 16:23:40 +0200

    update nnunet liver pipeline for the new DicomImporter module

--- models/nnunet_liver/scripts/run.py
+++ models/nnunet_liver/scripts/run.py
@@ -1,6 +1,7 @@
 """
 -------------------------------------------------
-MHub - run the PP pipeline locally
+MHub - run the NNUnet liver segmentation
+       pipeline
 -------------------------------------------------
 
 -------------------------------------------------
@@ -13,8 +14,7 @@ import sys
 sys.path.append('.')
 
 from mhubio.core import Config, DataType, FileType, CT, SEG
-from mhubio.modules.importer.UnsortedDicomImporter import UnsortedInstanceImporter
-from mhubio.modules.importer.DataSorter import DataSorter
+from mhubio.modules.importer.DicomImporter import DicomImporter
 from mhubio.modules.convert.NiftiConverter import NiftiConverter
 from mhubio.modules.runner.NNUnetRunner import NNUnetRunner
 from mhubio.modules.convert.DsegConverter import DsegConverter
@@ -22,21 +22,16 @@ from mhubio.modules.organizer.DataOrganizer import DataOrganizer
 
 # clean-up
 import shutil
-shutil.rmtree("/app/data/sorted", ignore_errors=True)
-shutil.rmtree("/app/data/nifti", ignore_errors=True)
+shutil.rmtree("/app/data/sorted_data", ignore_errors=True)
 shutil.rmtree("/app/tmp", ignore_errors=True)
 shutil.rmtree("/app/data/output_data", ignore_errors=True)
 
 # config
 config = Config('/app/models/nnunet_liver/config/config.yml')
 config.verbose = True  # TODO: define levels of verbosity and integrate consistently. 
-config.debug = False
 
-# import
-UnsortedInstanceImporter(config).execute()
-
-# sort
-DataSorter(config).execute()
+# import (ct:dicom)
+DicomImporter(config).execute()
 
 # convert (ct:dicom -> ct:nifti)
 NiftiConverter(config).execute()
@@ -44,8 +39,6 @@ NiftiConverter(config).execute()
 # execute model (nnunet)
 runner = NNUnetRunner(config)
 runner.input_type = DataType(FileType.NIFTI, CT)
-runner.nnunet_model = '3d_lowres'
-runner.nnunet_task = 'Task003_Liver'
 runner.execute()
 
 # convert (seg:nifti -> seg:dcm)
@@ -53,7 +46,7 @@ DsegConverter(config).execute()
 
 # organize data into output folder
 organizer = DataOrganizer(config, set_file_permissions=sys.platform.startswith('linux'))
-organizer.setTarget(DataType(FileType.NIFTI, CT), "/app/data/output_data/image.nii.gz")
-organizer.setTarget(DataType(FileType.NIFTI, SEG), "/app/data/output_data/liver.nii.gz")
-organizer.setTarget(DataType(FileType.DICOMSEG, SEG), "/app/data/output_data/liver.seg.dcm")
+organizer.setTarget(DataType(FileType.NIFTI, CT), "/app/data/output_data/[i:sid]/image.nii.gz")
+organizer.setTarget(DataType(FileType.NIFTI, SEG), "/app/data/output_data/[i:sid]/liver.nii.gz")
+organizer.setTarget(DataType(FileType.DICOMSEG, SEG), "/app/data/output_data/[i:sid]/liver.seg.dcm")
 organizer.execute()
