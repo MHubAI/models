50a29401b4773a397e09fcee8cb7f9c336617b30
silvandeleemput <silvandeleemput@gmail.com>
2023-08-29 17:23:28 +0200

    add reportexporter support and switch to panimg mhaconverter backend

--- models/gc_luna22_ismi_baseline/utils/Luna22IsmiBaselineRunner.py
+++ models/gc_luna22_ismi_baseline/utils/Luna22IsmiBaselineRunner.py
@@ -13,7 +13,7 @@ import json
 from pathlib import Path
 from typing import Optional
 
-from mhubio.core import Instance, InstanceData, IO, Module
+from mhubio.core import Instance, InstanceData, IO, Module, ValueOutput, ClassOutput, Meta
 
 import SimpleITK
 
@@ -22,14 +22,34 @@ from process import Nodule_classifier as NoduleClassifier
 import tensorflow as tf
 
 
+@ValueOutput.Name('malignancy_risk')
+@ValueOutput.Meta(Meta(key="value"))
+@ValueOutput.Label('MalignancyRisk')
+@ValueOutput.Type(float)
+@ValueOutput.Description('Probability of the nodule malignancy.')
+class MalignancyRiskOutput(ValueOutput):
+    pass
+
+
+@ClassOutput.Name('texture')
+@ClassOutput.Label('NoduleType')
+@ClassOutput.Description('Prediction of the nodule type.')
+@ClassOutput.Class(0, 'Non-solid', 'Non-solid.')
+@ClassOutput.Class(1, 'Part-solid', 'Part-solid.')
+@ClassOutput.Class(2, 'Solid', 'Solid.')
+class NoduleTypeOutput(ClassOutput):
+    pass
+
+
 class Luna22IsmiBaselineRunner(Module):
 
     _cached_classifier: Optional[NoduleClassifier] = None
 
     @IO.Instance()
     @IO.Input('in_data', 'mha|nrrd|nifti:mod=ct', the='input volume centered around a nodule from a lung CT image')
-    @IO.Output('out_data', 'nodule_predictions.json', 'json:model=Luna22IsmiBaseline', 'in_data', the='Luna22 Ismi nodule predictions (malignancy / nodule type)')
-    def task(self, instance: Instance, in_data: InstanceData, out_data: InstanceData) -> None:
+    @IO.OutputData('malignancy_risk', MalignancyRiskOutput, data='in_data', the='Luna22 Ismi nodule malignancy probability')
+    @IO.OutputData('texture', NoduleTypeOutput, data='in_data', the='Luna22 Ismi nodule type classification')
+    def task(self, instance: Instance, in_data: InstanceData, malignancy_risk: MalignancyRiskOutput, texture: NoduleTypeOutput) -> None:
         assert len(tf.config.list_physical_devices("GPU")) >= 1, \
             "Error: Luna22IsmiBaselineRunner must be run on a GPU! Because: " \
             "The Conv2D op currently only supports the NHWC tensor format on" \
@@ -41,5 +61,5 @@ class Luna22IsmiBaselineRunner(Module):
             self._cached_classifier = NoduleClassifier()
         classifier = self._cached_classifier
         predictions = classifier.predict(input_image=input_image)
-        with open(out_data.abspath, "w") as f:
-            json.dump(predictions, f, indent=4)
+        malignancy_risk.value = predictions["malignancy_risk"]
+        texture.value = predictions["texture"]
