ce43e32240e4456bbb53f2f581d23346c0869346
silvandeleemput <silvandeleemput@gmail.com>
2023-09-06 20:30:14 +0200

    add JSON reportexporter and support for ouputdata

--- models/gc_stoic_baseline/utils/StoicBaselineRunner.py
+++ models/gc_stoic_baseline/utils/StoicBaselineRunner.py
@@ -8,7 +8,7 @@ Author: Sil van de Leemput
 Email:  sil.vandeleemput@radboudumc.nl
 ------------------------------------------------------------
 """
-from mhubio.core import Instance, InstanceData, IO, Module
+from mhubio.core import Instance, InstanceData, IO, Module, ValueOutput
 
 import SimpleITK
 import json
@@ -16,14 +16,34 @@ import json
 from process import StoicAlgorithm
 
 
+@ValueOutput.Name('probability-covid-19')
+@ValueOutput.Meta(Meta(key="value"))
+@ValueOutput.Label('Covid19Probability')
+@ValueOutput.Type(float)
+@ValueOutput.Description('Probability of presence of Covid19.')
+class Covid19ProbabilityOutput(ValueOutput):
+    pass
+
+
+@ValueOutput.Name('probability-severe-covid-19')
+@ValueOutput.Meta(Meta(key="value"))
+@ValueOutput.Label('SevereCovid19Probability')
+@ValueOutput.Type(float)
+@ValueOutput.Description('Probability of presence of severe Covid19.')
+class SevereCovid19ProbabilityOutput(ValueOutput):
+    pass
+
+
 class StoicBaselineRunner(Module):
 
     @IO.Instance()
     @IO.Input('in_data', 'mha|nrrd|nifti:mod=ct', the='input lung CT')
-    @IO.Output('out_data', 'gc_stoic_baseline_covid_scores.json', 'json:model=StoicBaselineCovidScore', 'in_data', the='Stoic baseline covid 19 scores')
-    def task(self, instance: Instance, in_data: InstanceData, out_data: InstanceData) -> None:
+    @IO.OutputData('probability_covid_19', Covid19ProbabilityOutput, data='in_data', the='Stoic baseline Covid19 probability')
+    @IO.OutputData('probability_severe_covid_19', SevereCovid19ProbabilityOutput, data='in_data', the='Stoic baseline severe Covid19 probability')
+    def task(self, instance: Instance, in_data: InstanceData, probability_covid_19: Covid19ProbabilityOutput, probability_severe_covid_19: SevereCovid19ProbabilityOutput) -> None:
         input_image = SimpleITK.ReadImage(in_data.abspath)
         predictions = StoicAlgorithm().predict(input_image=input_image)
         predictions = {k.name:v for k,v in predictions.items()}
-        with open(out_data.abspath, "w") as f:
-            json.dump(predictions, f, indent=4)
+        # configure output data
+        probability_covid_19.value = predictions["probability-covid-19"]
+        probability_severe_covid_19.value = predictions["probability-severe-covid-19"]
