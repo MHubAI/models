b283d7d138b13d1c5e1bb19b76016f0fca364ef0
silvandeleemput <silvandeleemput@gmail.com>
2023-09-14 15:53:38 +0200

    Update and clean Node21 model related files
    * Dockerfile
      * Use mhubio.run as entrypoint
      * Fixed commit for node21 repo
      * Updated TODO on MHub model code integration
    * default.yml
      * Changed mhaconverter backend to panimg
    * Node21BaselineRunner.py
      * Added comments
    * Removed
      * run.py
      * PanImgConverters.py

--- models/gc_node21_baseline/utils/Node21BaselineRunner.py
+++ models/gc_node21_baseline/utils/Node21BaselineRunner.py
@@ -14,10 +14,12 @@ import SimpleITK
 import json
 from pathlib import Path
 
+# Import Node21 baseline nodule detection algorithm from the node21_detection_baseline repo
 from process import Noduledetection
 
 # TODO should move to mhubio/core/templates.py
-CR      = Meta(mode="CR")       # CR	Computed Radiography
+CR = Meta(mode="CR")  # CR	Computed Radiography
+
 
 class Node21BaselineRunner(Module):
 
@@ -25,8 +27,13 @@ class Node21BaselineRunner(Module):
     @IO.Input('in_data', 'mha|nrrd|nifti:mod=cr', the='input chest X-Ray')
     @IO.Output('out_data', 'nodules.json', 'json:model=Node21Baseline', 'in_data', the='Node21 baseline nodule prediction')
     def task(self, instance: Instance, in_data: InstanceData, out_data: InstanceData) -> None:
+        # Read input image
         input_image = SimpleITK.ReadImage(in_data.abspath)
+
+        # Run nodule detection algorithm on the input image and generate predictions
         tmp_path = Path("/app/tmp")
         predictions = Noduledetection(input_dir=tmp_path, output_dir=tmp_path).predict(input_image=input_image)
+
+        # Export the predictions to a JSON file
         with open(out_data.abspath, "w") as f:
             json.dump(predictions, f, indent=4)
--- models/gc_node21_baseline/utils/__init__.py
+++ models/gc_node21_baseline/utils/__init__.py
@@ -1,2 +1,1 @@
 from .Node21BaselineRunner import *
-from .PanImgConverters import *
