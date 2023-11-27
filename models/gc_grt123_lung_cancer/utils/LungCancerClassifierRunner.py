2d4365ad91a33be3bf86f666a1a4768d8e9dbc81
silvandeleemput <silvandeleemput@gmail.com>
2023-11-27 10:17:52 +0100

    added value output for overall score and added dynamic value output for all findings

--- models/gc_grt123_lung_cancer/utils/LungCancerClassifierRunner.py
+++ models/gc_grt123_lung_cancer/utils/LungCancerClassifierRunner.py
@@ -9,7 +9,7 @@ Email:  sil.vandeleemput@radboudumc.nl
 --------------------------------------------------------
 """
 import torch.cuda
-from mhubio.core import Instance, InstanceData, IO, Module
+from mhubio.core import Instance, InstanceData, IO, Module, ValueOutput, Meta
 
 from typing import Dict
 import json
@@ -19,6 +19,25 @@ import torch
 
 # Import the main module for the grt123 algorithm, which must be used for running the classification
 import main
+
+
+@ValueOutput.Name('lncancerprob')
+@ValueOutput.Meta(Meta(min=0.0, max=1.0, type="probability"))
+@ValueOutput.Label('Lung Nodule cancer probability score.')
+@ValueOutput.Type(float)
+@ValueOutput.Description('The predicted cancer probability score for a single lung nodule detected by the algorithm')
+class LNCancerProb(ValueOutput):
+   pass
+
+
+@ValueOutput.Name('clcancerprob')
+@ValueOutput.Meta(Meta(min=0.0, max=1.0, type="probability"))
+@ValueOutput.Label('Case level cancer probability score.')
+@ValueOutput.Type(float)
+@ValueOutput.Description('The predicted cancer probability score for the whole case')
+class CLCancerProb(ValueOutput):
+    pass
+
 
 # This method cleans the raw results from the grt123 algorithm output and only keeps the relevant details
 def cleanup_json_report(data: Dict):
@@ -46,8 +65,10 @@ class LungCancerClassifierRunner(Module):
 
     @IO.Instance()
     @IO.Input('in_data', 'mha:mod=ct', the='input ct scan')
-    @IO.Output('out_data', 'grt123_lung_cancer_findings.json', 'json:model=grt123LungCancerClassification', 'in_data', the='predicted nodules and lung cancer findings of the lung lobe')
-    def task(self, instance: Instance, in_data: InstanceData, out_data: InstanceData) -> None:
+    @IO.Output('out_data', 'grt123_lung_cancer_findings.json', 'json:model=grt123LungCancerClassification', data='in_data', the='predicted nodules and lung cancer findings of the lung lobe')
+    @IO.OutputData('clcancerprob', CLCancerProb, the='Case level probability score')
+    @IO.OutputDatas('lncancerprobs', LNCancerProb, the='Individual lung nodule probability scores')
+    def task(self, instance: Instance, in_data: InstanceData, out_data: InstanceData, clcancerprob: CLCancerProb, lncancerprobs: LNCancerProb) -> None:
         # create temporary directories for the preprocessed data and the cropped bounding boxes
         tmp_path = Path(self.config.data.requestTempDir('grt123'))
         tmp_output_bbox_dir = tmp_path / "bbox"
@@ -76,10 +97,23 @@ class LungCancerClassifierRunner(Module):
             data_filter=r".*.mha"
         )
 
-        # store classification results (original json file)
-        self.log(f"Writing classification results to {out_data.abspath}", "NOTICE")
+        # retrieve classification results
         assert len(results) > 0, "LungCancerClassifierRunner - Always expects at least one output report"
-        results_json = results[0].to_json()
-        cleanup_json_report(results_json)
+        results_dict = results[0].to_json()
+        cleanup_json_report(results_dict)
+
+        # export to JSON (original json file)
+        self.log(f"Writing classification results to {out_data.abspath}", "NOTICE")
         with open(out_data.abspath, "w") as f:
-            json.dump(results_json, f, indent=4)
+            json.dump(results_dict, f, indent=4)
+
+        # set output value for case level cancer probability
+        clcancerprob.value = results_dict["cancerinfo"]["casecancerprobability"]
+
+        # set output values for nodule level cancer probabilities
+        for finding in results_dict["findings"]:
+            nodule_cancer_prob = LNCancerProb()
+            nodule_cancer_prob.meta = Meta(id=finding['id'], x=finding['x'], y=finding['y'], z=finding['z'], )
+            nodule_cancer_prob.description += f" (for nodule {finding['id']} at location ({finding['x']}, {finding['y']}, {finding['z']}))"
+            nodule_cancer_prob.value = finding["cancerprobability"]
+            lncancerprobs.add(nodule_cancer_prob)
