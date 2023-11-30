4abafd57479aa8d5b562355e2af83f8da8c9f081
Miriam Groeneveld <miriam.groeneveld.diag@gmail.com>
2023-11-30 14:54:05 +0100

    PR comments on mata.json

--- models/gc_picai_baseline/utils/PicaiBaselineRunner.py
+++ models/gc_picai_baseline/utils/PicaiBaselineRunner.py
@@ -18,12 +18,11 @@ from mhubio.core import Instance, InstanceData, IO, Module, ValueOutput, ClassOu
 from process import csPCaAlgorithm as PicaiClassifier
 
 
-@ValueOutput.Name('prostate_cancer_probability')
-@ValueOutput.Meta(Meta(key="value"))
-@ValueOutput.Label('ProstateCancerProbability')
+@ValueOutput.Name('prostate_cancer_likelihood')
+@ValueOutput.Label('ProstateCancerLikelihood')
 @ValueOutput.Type(float)
-@ValueOutput.Description('Probability of case-level prostate cancer.')
-class ProstateCancerProbability(ValueOutput):
+@ValueOutput.Description('Likelihood of case-level prostate cancer.')
+class ProstateCancerLikelihood(ValueOutput):
     pass
 
 
@@ -33,10 +32,10 @@ class PicaiBaselineRunner(Module):
     @IO.Input('in_data_t2', 'mha:mod=mr:type=t2w', the='input T2 weighted prostate MR image')
     @IO.Input('in_data_adc', 'mha:mod=mr:type=adc', the='input ADC prostate MR image')
     @IO.Input('in_data_hbv', 'mha:mod=mr:type=hbv', the='input HBV prostate MR image')
-    @IO.Output('cancer_probability_json', 'cspca-case-level-likelihood.json', "json", bundle='model', the='output JSON file with PICAI baseline prostate cancer probability')
-    @IO.Output('cancer_detection_heatmap', 'cspca_detection_map.mha', "mha:mod=hm", bundle='model', the='output heatmap indicating prostate cancer probability')
-    @IO.OutputData('cancer_probability', ProstateCancerProbability, the='PICAI baseline prostate cancer probability')
-    def task(self, instance: Instance, in_data_t2: InstanceData, in_data_adc: InstanceData, in_data_hbv: InstanceData, cancer_probability_json: InstanceData, cancer_detection_heatmap: InstanceData, cancer_probability: ProstateCancerProbability) -> None:
+    @IO.Output('cancer_likelihood_json', 'cspca-case-level-likelihood.json', "json", bundle='model', the='output JSON file with PICAI baseline prostate cancer likelihood')
+    @IO.Output('cancer_detection_heatmap', 'cspca_detection_map.mha', "mha:mod=hm", bundle='model', the='output heatmap indicating prostate cancer likelihood')
+    @IO.OutputData('cancer_likelihood', ProstateCancerLikelihood, the='PICAI baseline prostate cancer likelihood')
+    def task(self, instance: Instance, in_data_t2: InstanceData, in_data_adc: InstanceData, in_data_hbv: InstanceData, cancer_likelihood_json: InstanceData, cancer_detection_heatmap: InstanceData, cancer_likelihood: ProstateCancerLikelihood) -> None:
         # Initialize classifier object
         classifier = PicaiClassifier()
 
@@ -49,20 +48,20 @@ class PicaiBaselineRunner(Module):
 
         # Specify output files
         classifier.cspca_detection_map_path = Path(cancer_detection_heatmap.abspath)
-        classifier.case_confidence_path = Path(cancer_probability_json.abspath)
+        classifier.case_confidence_path = Path(cancer_likelihood_json.abspath)
 
         # Run the classifier on the input images
         classifier.process()
 
-        # Extract cancer probability value from cancer_probability_file
-        if not Path(cancer_probability_json.abspath).is_file():
-            raise FileNotFoundError(f"Output file {cancer_probability_json.abspath} could not be found!")
+        # Extract cancer likelihood value from cancer_likelihood_file
+        if not Path(cancer_likelihood_json.abspath).is_file():
+            raise FileNotFoundError(f"Output file {cancer_likelihood_json.abspath} could not be found!")
 
-        with open(cancer_probability_json.abspath, "r") as f:
-            cancer_prob = float(json.load(f))
+        with open(cancer_likelihood_json.abspath, "r") as f:
+            cancer_lh = float(json.load(f))
 
-        if not (isinstance(cancer_prob, (float, int)) and (0.0 <= cancer_prob <= 1.0)):
-            raise ValueError(f"Cancer probability value should be a probability value, found: {cancer_prob}")
+        if not (isinstance(cancer_lh, (float, int)) and (0.0 <= cancer_lh <= 1.0)):
+            raise ValueError(f"Cancer likelihood value should be between 0 and 1, found: {cancer_lh}")
 
         # Output the predicted values
-        cancer_probability.value = cancer_prob
+        cancer_likelihood.value = cancer_lh
