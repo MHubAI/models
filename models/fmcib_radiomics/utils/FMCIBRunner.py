4cf674dcace8c568d95ff5d0cc8f80403baff322
LennyN95 <lenny@moontec.de>
2024-03-06 11:12:27 +0100

    update Dockerfile, outsource centroid extraction, add coords schema

--- models/fmcib_radiomics/utils/FMCIBRunner.py
+++ models/fmcib_radiomics/utils/FMCIBRunner.py
@@ -5,49 +5,64 @@ Email:  bspai@bwh.harvard.edu
 ---------------------------------------------------------
 """
 
-import json
-import torch
+import json, jsonschema, os
 from fmcib.models import fmcib_model 
 import SimpleITK as sitk
 from mhubio.core import Instance, InstanceData, IO, Module
-from fmcib.preprocessing import preprocess
 
+COORDS_SCHEMA_PATH = os.path.join(os.path.dirname(__file__), "coords.schema.json")
+
+def fmcib(input_dict: dict, json_output_file_path: str):
+    """Run the FCMIB pipeline.
+
+    Args:
+        input_dict (dict): The input dictionary containing the image path and the seed point coordinates.
+        json_output_file_path (str): The path were the features are exported to as a json file.
+    """
+    # model dependency imports
+    import torch
+    from fmcib.preprocessing import preprocess
+    
+    # initialize model
+    model = fmcib_model()
+    
+    # run model preroecessing
+    image = preprocess(input_dict)
+    image = image.unsqueeze(0)
+    
+    # run model inference
+    model.eval()
+    with torch.no_grad():
+        features = model(image)
+
+    # generate fearure dictionary
+    feature_dict = {f"feature_{idx}": feature for idx, feature in enumerate(features.flatten().tolist())}
+
+    # write feature dictionary to json file
+    with open(json_output_file_path, "w") as f:
+        json.dump(feature_dict, f)
 
 class FMCIBRunner(Module):
+    
     @IO.Instance()
     @IO.Input('in_data', 'nrrd:mod=ct', the='Input NRRD file')
-    @IO.Input('in_mask', 'nrrd|json', the='Tumor mask for the input NRRD file')
-    @IO.Output('feature_json', 'features.json', "json", bundle='model', the='output JSON file')
-    def task(self, instance: Instance, in_data: InstanceData, in_mask: InstanceData, feature_json: InstanceData) -> None:
-        mask_path = in_mask.abspath
-        mask = sitk.ReadImage(mask_path)
+    @IO.Input('centroids_json', 'json:type=fmcibcoordinates', the='The centroids in the input image coordinate space')
+    @IO.Output('feature_json', 'features.json', "json:type=fmcibfeatures", bundle='model', the='Features extracted from the input image at the specified seed point.')
+    def task(self, instance: Instance, in_data: InstanceData, centroids_json: InstanceData, feature_json: InstanceData) -> None:
+        
+        # read centroids from json file
+        centroids = json.load(centroids_json.abspath)
 
-        # Get the CoM of the mask
-        label_shape_filter = sitk.LabelShapeStatisticsImageFilter()
-        label_shape_filter.Execute(mask)
-        try:
-            centroid = label_shape_filter.GetCentroid(255)
-        except:
-            centroid = label_shape_filter.GetCentroid(1)
+        # verify input data schema
+        with open("models/fmcib_radiomics/utils/input_schema.json") as f:
+            schema = json.load(f)
+        jsonschema.validate(centroids, schema)
 
-        x, y, z = centroid
-
+        # define input dictionary
         input_dict = {
             "image_path": in_data.abspath,
-            "coordX": x,
-            "coordY": y,
-            "coordZ": z,
+            **centroids
         }
 
-        image = preprocess(input_dict)
-        image = image.unsqueeze(0)
-        model = fmcib_model()
-
-        model.eval()
-        with torch.no_grad():
-            features = model(image)
-
-        feature_dict = {f"feature_{idx}": feature for idx, feature in enumerate(features.flatten().tolist())}
-
-        with open(feature_json.abspath, "w") as f:
-            json.dump(feature_dict, f)
+        # run model
+        fmcib(input_dict, feature_json.abspath)
