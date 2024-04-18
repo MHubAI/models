269c1782fdb599d8e36fa22bbc31b709cf2dcfde
LennyN95 <lenny@moontec.de>
2024-04-18 21:55:55 +0200

    add custom mask path configurable to pyradiomics

--- models/pyradiomics/utils/PyRadiomicsRunner.py
+++ models/pyradiomics/utils/PyRadiomicsRunner.py
@@ -11,14 +11,19 @@ Date:   16.03.2024
 """
 
 from mhubio.core import Module, Instance, InstanceData, IO, InstanceDataCollection
-import SimpleITK as sitk, numpy as np, csv, os
+from mhubio.modules.organizer.DataOrganizer import DataOrganizer
+import SimpleITK as sitk, numpy as np, pandas as pd, csv, os
 
 @IO.Config('config_file', str, '/app/models/pyradiomics/res/params.yml', the='path to the pyradiomics parameter file')
 @IO.Config('z_align_seg_and_image', bool, False, the='align segmentation and image in their z direction')
+@IO.Config('mask_file_pattern', str, '[i:sid]/masks/[basename]', the='pattern to find the mask file')
+@IO.Config('mask_file_column_name', str, 'Mask', the='column name in the pyradiomics batch processing file')
 class PyRadiomicsRunner(Module):
 
     config_file: str
     z_align_seg_and_image: bool
+    mask_file_pattern: str
+    mask_file_column_name: str
 
     def align_seg_and_image(self, image_file: str, seg_file: str) -> str:
         
@@ -63,7 +68,7 @@ class PyRadiomicsRunner(Module):
 
     @IO.Instance()
     @IO.Input('image', 'nifti|nrrd:mod=ct|mr',  the='input image ct or mr scan')
-    @IO.Inputs('segmentation', 'nifti|nrrd:mod=seg:roi.length=1', the='input segmentation')
+    @IO.Inputs('segmentation', 'nifti|nrrd:mod=seg', the='input segmentation')
     @IO.Output('results', '[d:roi].pyradiomics.csv', 'csv:features=pyradiomics', data='image', the='output csv file with the results')
     def task(self, instance: Instance, image: InstanceData, segmentation: InstanceDataCollection, results: InstanceData) -> None:
 
@@ -71,25 +76,39 @@ class PyRadiomicsRunner(Module):
         tmp_dir = self.config.data.requestTempDir('pyradiomics_config')
         pyr_bp_file = os.path.join(tmp_dir, 'pyradiomics_batch.csv')
 
+        # mask column name
+        # NOTE: "Mask" is the reserved column name that points to the mask file for pyradiomics.
+        #       This path, however, only makes sense inside the docker container. To resolve this to outside masks, the
+        #       user can specify a parttern (and use the same pattern in the DataOrganizer) and write this into a separate 
+        #       column (as specified by mask_file_column_name). If mask_file_column_name is "Mask" we will write it as "_Mask"
+        #       remporarily and then later replace "Mask" with "_Mask"
+        if self.mask_file_column_name == "Mask":
+            mask_column_name = "_Mask"
+        else:
+            mask_column_name = self.mask_file_column_name
+
         # prepare csv for pyradiomics batch processing
         # NOTE: we could allow custom columns that extract any metadata specified from the config but 
         #       for now we simply include the metadata string from each segmentation file
         with open(pyr_bp_file, 'w') as csvfile:
             writer = csv.writer(csvfile)
-            writer.writerow(['Image', 'Mask', 'Label', 'Label_channel', 'MHub ROI', 'MHub Metadata'])
+            writer.writerow(['Image', 'Mask', 'Label', 'Label_channel', 'MHub ROI', 'MHub Metadata', mask_column_name])
             
             for seg_file in segmentation:
                 seg_rois = seg_file.type.meta['roi'].split(',')
-          
+            
                 # optionally align nifti file 
                 if self.z_align_seg_and_image:
                     seg_file_abspath = self.align_seg_and_image(image.abspath, seg_file.abspath)
                 else:
                     seg_file_abspath = seg_file.abspath
                     
+                # get mask file outside-mhub-path (from a pattern, which must be used in the DataOrganizer too then)
+                seg_file_resolved_path = DataOrganizer.resolveTarget(self.mask_file_pattern, seg_file)
+                    
                 # write one row per roi into the pyradiomics batch processing csv
                 for channel_id, seg_roi in enumerate(seg_rois):
-                    writer.writerow([image.abspath, seg_file_abspath, 1, channel_id + 1, seg_roi, seg_file.type.meta])
+                    writer.writerow([image.abspath, seg_file_abspath, 1, channel_id + 1, seg_roi, seg_file.type.meta, seg_file_resolved_path])
 
         # for debugging print the content of the pyr_bp_file 
         with open(pyr_bp_file, 'r') as f:
@@ -110,3 +129,9 @@ class PyRadiomicsRunner(Module):
         # run pyradiomics
         self.subprocess(cmd)
         
+        # check if we need to replace the "Mask" column with the contents in the "_Mask" column
+        if mask_column_name == "_Mask":
+            df = pd.read_csv(results.abspath)
+            df['Mask'] = df['_Mask']
+            df.drop(columns=['_Mask'], inplace=True)
+            df.to_csv(results.abspath, index=False)
