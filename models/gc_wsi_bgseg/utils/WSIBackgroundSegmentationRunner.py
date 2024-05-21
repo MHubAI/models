5b35f8c3773f7338203bac6eab29c0aa258f1868
silvandeleemput <silvandeleemput@gmail.com>
2024-05-21 15:37:12 +0200

    runner - add spacing configuration options

--- models/gc_wsi_bgseg/utils/WSIBackgroundSegmentationRunner.py
+++ models/gc_wsi_bgseg/utils/WSIBackgroundSegmentationRunner.py
@@ -8,14 +8,35 @@ Author: Sil van de Leemput
 Email:  sil.vandeleemput@radboudumc.nl
 ------------------------------------------------------------------
 """
+from typing import Optional
+
 import tempfile
 
 from mhubio.core import Instance, InstanceData, IO, Module
 
 from pathlib import Path
 
-
+@IO.Config(
+    'input_spacing', float, 2.0,
+    the="Desired input spacing to run the segmentation algorithm for. "
+        "The closest level matching the spacing in the input Tiff image will be selected. "
+        "Default is 2.0 micrometer."
+)
+@IO.Config(
+    'output_spacing', Optional[float], None,
+    the="Desired output spacing for the output segmentation. "
+        "By default this matches the input_spacing."
+)
+@IO.Config(
+    'spacing_tolerance', float, 0.25,
+    the="Relative spacing tolerance with respect to the desired input_spacing. "
+        "By default this is set to 25%."
+)
 class WSIBackgroundSegmentationRunner(Module):
+
+    input_spacing: float
+    output_spacing: Optional[float]
+    spacing_tolerance: float
 
     CLI_SCRIPT_PATH = Path("/app") / "src" / "process.py"
 
@@ -23,6 +44,10 @@ class WSIBackgroundSegmentationRunner(Module):
     @IO.Input('in_data', 'tif|tiff:mod=sm', the='input whole slide image Tiff')
     @IO.Output('out_data', 'gc_wsi_bg_segmentation.tif', 'tiff:mod=seg:model=WSIBackgroundSegmentation', 'in_data', the='Background segmentation of the input WSI.')
     def task(self, instance: Instance, in_data: InstanceData, out_data: InstanceData) -> None:
+        output_spacing = self.output_spacing
+        if self.output_spacing is None:
+            output_spacing = self.input_spacing
+
         with tempfile.TemporaryDirectory() as tmp_dir:
             # Execute the Tiger LB2 Algorithm through a Python subprocess and associated pipenv environment
             self.subprocess(
@@ -34,7 +59,13 @@ class WSIBackgroundSegmentationRunner(Module):
                     in_data.abspath,
                     out_data.abspath,
                     "--work-dir",
-                    tmp_dir
+                    tmp_dir,
+                    "--input-spacing",
+                    str(self.input_spacing),
+                    "--output-spacing",
+                    str(output_spacing),
+                    "--spacing-tolerance",
+                    str(self.spacing_tolerance)
                 ]
             )
 
