b972102f0e4948d0b4b34175eb8ea3fe5082f13e
Rahul Soni <rahul.soni@bamfhealth.com>
2024-06-07 11:35:11 +0530

    added meta data and refactored post processor

--- models/bamf_nnunet_mr_breast/utils/BreastPostProcessor.py
+++ models/bamf_nnunet_mr_breast/utils/BreastPostProcessor.py
@@ -14,6 +14,8 @@ import os, shutil
 import SimpleITK as sitk
 import numpy as np
 from skimage import measure, filters
+from typing import Union
+from pathlib import Path
 
 
 class BreastPostProcessor(Module):
@@ -24,7 +26,10 @@ class BreastPostProcessor(Module):
         seg_data[seg_data > 0] = 1
         return seg_data
 
-    def n_connected(self, img_data):
+    def n_connected(self, img_data: sitk.Image) -> sitk.Image:
+        """
+        Analyse connected components and drop smaller blobs
+        """
         img_data_mask = np.zeros(img_data.shape)
         img_data_mask[img_data >= 1] = 1
         img_filtered = np.zeros(img_data_mask.shape)
@@ -44,43 +49,19 @@ class BreastPostProcessor(Module):
         img_data[img_filtered != 1] = 0
         return img_data
 
-    def n4_bias_correction(
-        input_image,
-        shrink_factor=1,
-        mask_image=None,
-        number_of_iterations=50,
-        number_of_fitting_levels=4,
-    ):
-        if mask_image is None:
-            mask_image = sitk.OtsuThreshold(input_image, 0, 1, 200)
-
-        if shrink_factor > 1:
-            input_image = sitk.Shrink(
-                input_image, [shrink_factor] * input_image.GetDimension()
-            )
-            mask_image = sitk.Shrink(
-                mask_image, [shrink_factor] * mask_image.GetDimension()
-            )
-
-        corrector = sitk.N4BiasFieldCorrectionImageFilter()
-        corrector.SetMaximumNumberOfIterations(
-            [number_of_iterations] * number_of_fitting_levels
-        )
-
-        corrector.Execute(input_image, mask_image)
-        log_bias_field = corrector.GetLogBiasFieldAsImage(input_image)
-        corrected_image_full_resolution = input_image / sitk.Exp(log_bias_field)
-        return corrected_image_full_resolution
-
-    def get_seg_img(self, breast_and_fgt, breast_tumor, mr_path):
-        seg_data = np.zeros(breast_and_fgt.shape)
-        seg_data[breast_and_fgt == 1] = 1
-        seg_data[breast_tumor == 1] = 2
+    def merge_segmentations(self, breast_and_fgt_seg:sitk.Image, tumor_seg:sitk.Image, mr_path:Union[str,Path]) -> sitk.Image:
+        """
+        Merge labels of breast, fgt, and breast tumor
+        breast_and_fgt_seg: contains label=1 for breast, and label=2 for fgt
+        tumor_seg: assign label=3 for breast tumor in the merge image
+        returns: sitk image containing labels: [0,1,2,3]
+        """
+        tumor_seg[breast_and_fgt_seg == 0] = 0 # only focusing on predictions within the breast and fgt region
+        breast_and_fgt_seg[tumor_seg == 1] = 3
         ref = sitk.ReadImage(mr_path)
-        seg_img = sitk.GetImageFromArray(seg_data)
+        seg_img = sitk.GetImageFromArray(breast_and_fgt_seg)
         seg_img.CopyInformation(ref)
         return seg_img
-
 
     @IO.Instance()
     @IO.Input('in_breast_and_fgt_data', 'nifti:mod=seg:nnunet_task=Dataset009_Breast', the='input data from breast and fgt segmentation')
@@ -95,21 +76,24 @@ class BreastPostProcessor(Module):
         print('input breast tumor segmentation: ' + in_breast_tumor_data.abspath)
         print('output path: ' + out_data.abspath)
 
-        breast_fgt_seg = self.get_mask(in_breast_and_fgt_data.abspath)
-        breast_fgt_seg = self.n_connected(breast_fgt_seg)
+        # Breast and FGT segmentation
+        breast_and_fgt_seg = self.get_mask(in_breast_and_fgt_data.abspath)
+        breast_and_fgt_seg = self.n_connected(breast_and_fgt_seg)
 
-        breast_tumor_seg = self.get_mask(in_breast_tumor_data.abspath)
-        breast_tumor_seg[breast_fgt_seg == 0] = 0
+        # Breast tumor segmentation
+        tumor_seg = self.get_mask(in_breast_tumor_data.abspath)
+        tumor_seg = self.n_connected(tumor_seg)
 
-        seg_img = self.get_seg_img(
-            breast_and_fgt = np.copy(breast_fgt_seg),
-            breast_tumor = np.copy(breast_tumor_seg),
+        # Merged segmentation masks
+        output_seg = self.merge_segmentations(
+            breast_and_fgt = np.copy(breast_and_fgt_seg),
+            breast_tumor = np.copy(tumor_seg),
             mr_path = in_mr_data.abspath
             )
         process_dir = self.config.data.requestTempDir(label="nnunet-breast-processor")
-        process_file = os.path.join(process_dir, f'final.nii.gz')
+        process_file = os.path.join(process_dir, f'bamf_nnunet_mr_breast.nii.gz')
         sitk.WriteImage(
-            seg_img,
+            output_seg,
             process_file,
         )
         shutil.copyfile(process_file, out_data.abspath)
