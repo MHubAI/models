5704c2982bf7979c33c7c830120b873b25fe44e4
Suraj Pai <bspai@bwh.harvard.edu>
2024-07-15 16:49:52 +0000

    Fix arg name

--- models/bpreg/utils/BPRRunner.py
+++ models/bpreg/utils/BPRRunner.py
@@ -17,9 +17,9 @@ class BPRRunner(Module):
     @IO.Instance()
     @IO.Input('in_data', 'nifti:mod=ct', the='The input NIfTI CT image file.')
     @IO.Output('bpreg_json', 'bpreg.json', "json:type=bpreg", bundle='model', the=' The output JSON file to save the extracted features.')
-    def task(self, instance: Instance, in_data: InstanceData, utility_json: InstanceData) -> None:
+    def task(self, instance: Instance, in_data: InstanceData, bpreg_json: InstanceData) -> None:
         gpu_available = torch.cuda.is_available()
         model = InferenceModel("/app/public_inference_model/public_bpr_model/", gpu=gpu_available)
         input_path = in_data.abspath
-        output_path = utility_json.abspath
+        output_path = bpreg_json.abspath
         model.nifti2json(input_path, output_path, stringify_json=False)
