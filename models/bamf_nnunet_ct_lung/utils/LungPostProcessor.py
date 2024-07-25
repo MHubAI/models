b84f9c71ef655539a8debe3e348b9f9126c1bec2
jithenece <jithenece@gmail.com>
2024-07-25 21:17:52 +0530

    fix review comments

--- models/bamf_nnunet_ct_lung/utils/LungPostProcessor.py
+++ models/bamf_nnunet_ct_lung/utils/LungPostProcessor.py
@@ -9,21 +9,37 @@ Email:  jithendra.kumar@bamfhealth.com
 """
 from mhubio.core import IO
 from mhubio.core import Module, Instance, InstanceData
-import os, shutil
 import SimpleITK as sitk
 import numpy as np
-from skimage import measure, filters
+from skimage import measure
 
 
 class LungPostProcessor(Module):
 
-    def get_lung(self, ip_path):
-        # get lung segmentation
+    def perform_binary_threshold_segmentation(self, ip_path):
+        """
+        Perform binary threshold segmentation on the input image.
+
+        Args:
+        - ip_path (str): Path to the input image file.
+
+        Returns:
+        - numpy.ndarray: Segmented binary mask where non-zero values represent the segmented region.
+        """
         seg_data = sitk.GetArrayFromImage(sitk.ReadImage(ip_path))
         seg_data[seg_data > 0] = 1
         return seg_data
 
-    def n_connected(self, img_data):
+    def extract_largest_connected_component(self, img_data):
+        """
+        Extract the single largest connected component from the segmentation image data.
+
+        Args:
+        - img_data (numpy.ndarray): Segmentation image data where connected components are to be identified.
+
+        Returns:
+        - numpy.ndarray: Binary image data with only the largest connected component retained.
+        """
         img_data_mask = np.zeros(img_data.shape)
         img_data_mask[img_data >= 1] = 1
         img_filtered = np.zeros(img_data_mask.shape)
@@ -37,14 +53,24 @@ class LungPostProcessor(Module):
 
         for key, value in sorted_dict.items():
             if count >= 1 and count <= 2:
-                print(key, value)
                 img_filtered[blobs_labels == key] = 1
             count += 1
 
         img_data[img_filtered != 1] = 0
         return img_data
 
-    def get_seg_img(self, lungs, nodules, ct_path):
+    def create_segmentation_image(self, lungs, nodules, ct_path):
+        """
+        Create a segmentation image combining lung and nodule segmentations.
+
+        Args:
+        - lungs (numpy.ndarray): Binary mask of lung segmentation.
+        - nodules (numpy.ndarray): Binary mask of nodule segmentation.
+        - ct_path (str): Path to the original CT image used as reference.
+
+        Returns:
+        - SimpleITK.Image: Segmentation image where lung and nodule regions are labeled as 1 and 2, respectively.
+        """
         seg_data = np.zeros(lungs.shape)
         seg_data[lungs == 1] = 1
         seg_data[nodules == 1] = 2
@@ -62,20 +88,13 @@ class LungPostProcessor(Module):
     def task(self, instance: Instance, in_rg_data: InstanceData, in_nodules_data: InstanceData,
              in_ct_data: InstanceData, out_data: InstanceData):
 
-        print('input nodules data: ' + in_nodules_data.abspath)
-        print('store results in ' + out_data.abspath)
+        self.v('running LungPostProcessor')
 
-        seg_data = self.get_lung(in_rg_data.abspath)
-        lungs = self.n_connected(seg_data)
+        seg_data = self.perform_binary_threshold_segmentation(in_rg_data.abspath)
+        lungs = self.extract_largest_connected_component(seg_data)
 
-        nodules = self.get_lung(in_nodules_data.abspath)
+        nodules = self.perform_binary_threshold_segmentation(in_nodules_data.abspath)
         nodules[lungs == 0] = 0
 
-        seg_img = self.get_seg_img(np.copy(lungs), np.copy(nodules), in_ct_data.abspath)
-        process_dir = self.config.data.requestTempDir(label="nnunet-lung-processor")
-        process_file = os.path.join(process_dir, f'final.nii.gz')
-        sitk.WriteImage(
-            seg_img,
-            process_file,
-        )
-        shutil.copyfile(process_file, out_data.abspath)
+        final_seg_img = self.create_segmentation_image(np.copy(lungs), np.copy(nodules), in_ct_data.abspath)
+        sitk.WriteImage(final_seg_img,out_data.abspath)
