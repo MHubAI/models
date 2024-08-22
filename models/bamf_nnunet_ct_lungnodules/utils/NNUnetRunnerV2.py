cbdbbd7d047bfeebe558b5bbbd4a77e337b85b9d
jithenece <jithenece@gmail.com>
2024-08-22 14:49:28 +0530

    fix code review comments

--- models/bamf_nnunet_ct_lungnodules/utils/NNUnetRunnerV2.py
+++ models/bamf_nnunet_ct_lungnodules/utils/NNUnetRunnerV2.py
@@ -1,50 +1,36 @@
 """
 -------------------------------------------------
 MHub - NNU-Net Runner v2
-       Runner for pre-trained nnunet v2 models. 
+       Custom Runner for pre-trained nnunet v2 models.
 -------------------------------------------------
 
 -------------------------------------------------
-Author: Rahul Soni
-Email:  rahul.soni@bamfhealth.com
+Author: Jithendra Kumar
+Email:  jithendra.kumar@bamfhealth.com
 -------------------------------------------------
 """
 
 
-from typing import List, Optional
-import os, subprocess, shutil
-import SimpleITK as sitk, numpy as np
+import os, shutil
 from mhubio.core import Module, Instance, InstanceData, DataType, FileType, IO
 
 
-nnunet_dataset_name_regex = r"Dataset[0-9]{3}_[a-zA-Z0-9_]+"
 
-@IO.ConfigInput('in_data', 'nifti:mod=mr', the="input data to run nnunet on")
-@IO.Config('nnunet_dataset', str, None, the='nnunet dataset name')
-@IO.Config('nnunet_config', str, None, the='nnunet model name (2d, 3d_lowres, 3d_fullres, 3d_cascade_fullres)')
-@IO.Config('folds', int, None, the='number of folds to run nnunet on')
-@IO.Config('use_tta', bool, True, the='flag to enable test time augmentation')
-@IO.Config('roi', str, None, the='roi or comma separated list of roi the nnunet segments')
+@IO.ConfigInput('in_data', 'nifti:mod=ct', the="input data to run nnunet on")
 class NNUnetRunnerV2(Module):
 
-    nnunet_dataset: str
-    nnunet_config: str
+    nnunet_dataset: str = 'Dataset007_Nodules'
+    nnunet_config: str = '3d_fullres'
     input_data_type: DataType
-    folds: int                          # TODO: support optional config attributes
-    use_tta: bool
-    roi: str
 
     @IO.Instance()
     @IO.Input("in_data", the="input data to run nnunet on")
-    @IO.Output("out_data", 'VOLUME_001.nii.gz', 'nifti:mod=seg:model=nnunet', data='in_data', the="output data from nnunet")
+    @IO.Output("out_data", 'VOLUME_001.nii.gz', 'nifti:mod=seg:model=nnunet:roi=LUNG,LUNG+NODULE', data='in_data', the="output data from nnunet")
     def task(self, instance: Instance, in_data: InstanceData, out_data: InstanceData) -> None:
         
         # get the nnunet model to run
-        self.v("Running nnUNet_predict.")
-        self.v(f" > dataset:     {self.nnunet_dataset}")
-        self.v(f" > config:      {self.nnunet_config}")
+        self.v("Running nnUNetv2_predict.")
         self.v(f" > input data:  {in_data.abspath}")
-        self.v(f" > output data: {out_data.abspath}")
 
         # download weights if not found
         # NOTE: only for testing / debugging. For productiio always provide the weights in the Docker container.
@@ -53,10 +39,6 @@ class NNUnetRunnerV2(Module):
             bash_command = ["nnUNet_download_pretrained_model", self.nnunet_dataset]
             self.subprocess(bash_command, text=True)
 
-        # bring input data in nnunet specific format
-        # NOTE: only for nifti data as we hardcode the nnunet-formatted-filename (and extension) for now.
-        assert in_data.type.ftype == FileType.NIFTI
-        assert in_data.abspath.endswith('.nii.gz')
         inp_dir = self.config.data.requestTempDir(label="nnunet-model-inp")
         inp_file = f'VOLUME_001_0000.nii.gz'
         shutil.copyfile(in_data.abspath, os.path.join(inp_dir, inp_file))
@@ -71,11 +53,6 @@ class NNUnetRunnerV2(Module):
         #       structure. This is not the case for the mhub data structure. So we create a symlink to the input data
         #       in the nnunet input folder structure.
         os.symlink(os.path.join(os.environ['WEIGHTS_FOLDER'], self.nnunet_dataset), os.path.join(out_dir, self.nnunet_dataset))
-        
-        # NOTE: instead of running from commandline this could also be done in a pythonic way:
-        #       `nnUNet/nnunet/inference/predict.py` - but it would require
-        #       to set manually all the arguments that the user is not intended
-        #       to fiddle with; so stick with the bash executable
 
         # construct nnunet inference command
         bash_command  = ["nnUNetv2_predict"]
@@ -83,25 +60,10 @@ class NNUnetRunnerV2(Module):
         bash_command += ["-o", str(out_dir)]
         bash_command += ["-d", self.nnunet_dataset]
         bash_command += ["-c", self.nnunet_config]
-        
-        # add optional arguments
-        if self.folds is not None:
-            bash_command += ["-f", str(self.folds)]
-
-        if not self.use_tta:
-            bash_command += ["--disable_tta"]
 
         self.v(f" > bash_command:     {bash_command}")
         # run command
         self.subprocess(bash_command, text=True)
-
-        # output meta
-        meta = {
-            "model": "nnunet",
-            "nnunet_dataset": self.nnunet_dataset,
-            "nnunet_config": self.nnunet_config,
-            "roi": self.roi
-        }
 
         # get output data
         out_file = f'VOLUME_001.nii.gz'
@@ -109,6 +71,3 @@ class NNUnetRunnerV2(Module):
 
         # copy output data to instance
         shutil.copyfile(out_path, out_data.abspath)
-
-        # update meta dynamically
-        out_data.type.meta += meta
