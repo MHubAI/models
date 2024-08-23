d382e5fbe8cc1186dacbba1b8e6f5c4dd80943df
jithenece <jithenece@gmail.com>
2024-08-23 23:03:22 +0530

    remove custom seg entries

--- models/bamf_mr_brain_tumor/utils/InverseRegistrationRunner.py
+++ models/bamf_mr_brain_tumor/utils/InverseRegistrationRunner.py
@@ -31,7 +31,7 @@ class InverseRegistrationRunner(Module):
     @IO.Inputs('in_seg_datas', the="data to be converted")
     @IO.Inputs('in_mat_datas', the="transformation matrix data")
     @IO.Inputs('in_registration_datas', the="registered data")
-    @IO.Outputs('out_datas', path='[filename].nii.gz', dtype='nifti:task=inverse:roi=NECROSIS,EDEMA,ENHANCING',
+    @IO.Outputs('out_datas', path='[filename].nii.gz', dtype='nifti:task=inverse:roi=NECROSIS,EDEMA,ENHANCING_LESION',
                 data='in_registration_datas', bundle='inverse_t1c_registration', auto_increment=True, the="converted data")
     def task(self, instance: Instance, in_seg_datas : InstanceDataCollection, in_mat_datas: InstanceDataCollection, in_registration_datas: InstanceDataCollection,  out_datas: InstanceDataCollection, **kwargs) -> None:
 
@@ -54,55 +54,52 @@ class InverseRegistrationRunner(Module):
             in_seg_data = in_seg_datas.get(i)
 
             # check datatype 
-            if in_data.type.ftype == FileType.NIFTI:                
-                reverse_transformation_matrix = os.path.join(process_dir, f'{str(uuid.uuid4())}_reverse.mat')
-                reverse_transformation_file = os.path.join(process_dir, f'{str(uuid.uuid4())}.nii.gz')
-                # Command to convert the transformation matrices
-                convert_command = [
-                    "convert_xfm",
-                    "-omat",
-                    str(reverse_transformation_matrix),
-                    "-inverse",
-                    str(in_mat.abspath),
-                ]
-                
-                try:
-                    self.v("Converting transformation matrices...",convert_command)
-                    self.subprocess(convert_command, check=True)
-                    self.v("Transformation matrices converted successfully.")
-                except subprocess.CalledProcessError as e:
-                    self.v("Error converting transformation matrices:", e)
-                # Inverse registration using FLIRT with the saved transformation matrix
-                cmd = [
-                    "flirt",
-                    "-in",
-                    str(in_seg_data.abspath),
-                    "-ref",
-                    str(in_data.abspath),
-                    "-out",
-                    str(reverse_transformation_file),
-                    "-init",
-                    str(
-                        reverse_transformation_matrix
-                    ),  # Use the saved transformation matrix for inverse registration
-                    "-cost",
-                    "normmi",
-                    "-dof",
-                    "12",  # 6 degrees of freedom for rigid registration
-                    "-interp",
-                    "nearestneighbour",  # Interpolation method (adjust as needed)
-                    "-applyxfm",
-                ]
-                self.v("inverse transformation flirt...",cmd)
-                self.subprocess(cmd, check=True)
-                # Load your image
-                image = sitk.ReadImage(reverse_transformation_file)
+            reverse_transformation_matrix = os.path.join(process_dir, f'{str(uuid.uuid4())}_reverse.mat')
+            reverse_transformation_file = os.path.join(process_dir, f'{str(uuid.uuid4())}.nii.gz')
+            # Command to convert the transformation matrices
+            convert_command = [
+                "convert_xfm",
+                "-omat",
+                str(reverse_transformation_matrix),
+                "-inverse",
+                str(in_mat.abspath),
+            ]
 
-                # Change the label from 4 to 3
-                label_map = {4: 3}
-                changed_image = sitk.ChangeLabel(image, changeMap=label_map)
+            try:
+                self.v("Converting transformation matrices...",convert_command)
+                self.subprocess(convert_command, check=True)
+                self.v("Transformation matrices converted successfully.")
+            except subprocess.CalledProcessError as e:
+                self.v("Error converting transformation matrices:", e)
+            # Inverse registration using FLIRT with the saved transformation matrix
+            cmd = [
+                "flirt",
+                "-in",
+                str(in_seg_data.abspath),
+                "-ref",
+                str(in_data.abspath),
+                "-out",
+                str(reverse_transformation_file),
+                "-init",
+                str(
+                    reverse_transformation_matrix
+                ),  # Use the saved transformation matrix for inverse registration
+                "-cost",
+                "normmi",
+                "-dof",
+                "12",  # 6 degrees of freedom for rigid registration
+                "-interp",
+                "nearestneighbour",  # Interpolation method (adjust as needed)
+                "-applyxfm",
+            ]
+            self.v("inverse transformation flirt...",cmd)
+            self.subprocess(cmd, check=True)
+            # Load your image
+            image = sitk.ReadImage(reverse_transformation_file)
 
-                # Add the converted data to the output collection
-                sitk.WriteImage(changed_image, out_data.abspath)
-            else:
-                raise ValueError(f"CONVERT ERROR: unsupported file type {in_data.type.ftype}.")
+            # Change the label from 4 to 3
+            label_map = {4: 3}
+            changed_image = sitk.ChangeLabel(image, changeMap=label_map)
+
+            # Add the converted data to the output collection
+            sitk.WriteImage(changed_image, out_data.abspath)
