84a6e0f62f89a1d15e11034ecfe9678ea1247d2d
locastre <34068528+locastre@users.noreply.github.com>
2025-04-18 13:10:12 -0400

    Update SMITRunner.py

--- models/msk_smit_lung_gtv/utils/SMITRunner.py
+++ models/msk_smit_lung_gtv/utils/SMITRunner.py
@@ -15,7 +15,7 @@ from mhubio.core import Instance, InstanceData, IO, Module
 #from mhubio.modules.runner.ModelRunner import ModelRunner
 
 
-CLI_CWD = Path(__file__).parents[1] + '/src'
+CLI_CWD = str(Path(__file__).parents[1]) + '/src'
 
 
 # Optional config parameter/s examples noted below
@@ -28,7 +28,7 @@ class SMITRunner(Module):
     
     @IO.Instance()
     @IO.Input('in_data', the='input ct scan')
-    @IO.Output('out_data', 'gtv_mask.nii.gz', 'nifti:mod=seg:model=SMIT:roi=LUNG+NEOPLASM_MALIGNANT_PRIMARY', the='predicted segmentation of lung GTV')
+    @IO.Output('out_data', 'model_Mhub_dicom.nii.gz', 'nifti:mod=seg:model=SMIT:roi=LUNG+NEOPLASM_MALIGNANT_PRIMARY', the='predicted segmentation of lung GTV')
    
 
     def task(self, instance: Instance, in_data: InstanceData, out_data: InstanceData) -> None:   
@@ -37,15 +37,23 @@ class SMITRunner(Module):
         
         # bash command for SMIT
         #bash_command  = f"source " + condaEnvActivateScript + " && source " + wrapperPath + " " + sessiondir + " " + sessiondir + " " + load_weight_name + " " + scan.abspath
+        #"source", wrapperName, in_data.abspath, out_data.abspath, weightsName
+        data_dir = os.path.dirname(in_data.abspath)
+        save_folder = os.path.dirname(out_data.abspath)
 
-        cmd = [
-            "uv","run","-p","/app/.venv39","source", wrapperName, in_data.abspath, out_data.abspath, weightsName
+        cmd1 = ["uv","run","-p","/app/.venv39","python","utils/gen_data_json.py",in_data.abspath]
 
-        ]
+        self.subprocess(cmd1, cwd=CLI_CWD,text=True)
+
+        cmd2 = [
+            "uv","run","-p","/app/.venv39",
+            "python","run_segmentation.py","--roi_x","128","--roi_y","128","--roi_z","128",
+            "--space_x","1.5","--space_y","1.5","--space_z","2.0","--data_dir",data_dir,"--out_channels","2",
+            "--load_weight_name","trained_weights/model_Mhub.pt","--save_folder",save_folder,"--a_min=-500","--a_max=500","--use_smit","1"]
 
         # Display command on terminal
         self.log("Running SMIT")
         #self.log(">> ".join(bash_command))
         
         # run SMIT
-        self.subprocess(cmd, cwd = CLI_CWD, shell=True, text=True)
+        self.subprocess(cmd2, cwd = CLI_CWD, text=True)
